5e56109f460ef31d47771d37f0586ec389ebffbe
kevstevie <jianna442@gmail.com>
2026-04-06 02:07:12 +0900

    feat: solve #198 - House Robber (Medium) [python3]

---
+++ 0198-house-robber/solution.py
@@ -0,0 +1,18 @@
+# Problem: #198 - House Robber
+# Difficulty: Medium
+# Language: Python3
+# URL: https://leetcode.com/problems/house-robber/
+# Submitted: 2026-04-05
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        ans = nums[0]
+        dp = [[0] * 2 for _ in range(len(nums))]
+
+        dp[0][1] = nums[0]
+
+        for i in range(1, len(nums)):
+            dp[i][1] = dp[i - 1][0] + nums[i]
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+            ans = max(dp[i][1], dp[i][0], ans)
+            
+        return ans
