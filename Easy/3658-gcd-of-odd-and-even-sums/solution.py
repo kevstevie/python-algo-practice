193447fcaf261bbb3c8f9ac510d2ac1de50872d4
kevstevie <jianna442@gmail.com>
2026-07-15 20:51:19 +0900

    feat: solve #3658 - GCD of Odd and Even Sums (Easy) [python3] [Math, Number Theory]

---
+++ Easy/3658-gcd-of-odd-and-even-sums/solution.py
@@ -0,0 +1,9 @@
+# Problem: #3658 - GCD of Odd and Even Sums
+# Difficulty: Easy
+# Language: Python3
+# URL: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+# Submitted: 2026-07-15
+# Tags: Math, Number Theory
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
