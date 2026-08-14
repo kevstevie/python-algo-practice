0fe0f4161f92071a6af3943855da6bc77f278197
kevstevie <jianna442@gmail.com>
2026-08-14 14:37:20 +0900

    feat: solve #2958 - Length of Longest Subarray With at Most K Frequency (Medium) [python3] [Array, Hash Table, Sliding Window]

--- Medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.py
+++ Medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.py
@@ -2,7 +2,7 @@
 # Difficulty: Medium
 # Language: Python3
 # URL: https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/
-# Submitted: 2026-08-12
+# Submitted: 2026-08-14
 # Tags: Array, Hash Table, Sliding Window
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
@@ -11,11 +11,11 @@ class Solution:
         cnt = {}
         cnt[nums[0]] = 1
         ans = 1
-        max_freq = [1]
+        prev = nums[0]
         n = len(nums)
         
         while r < n:
-            max_val = max_freq[-1]
+            max_val = cnt[prev]
             if max_val <= k:
                 ans = max(ans, r - l + 1)
                 r += 1
@@ -23,10 +23,8 @@ class Solution:
                     break
                 cnt[nums[r]] = cnt.get(nums[r], 0) + 1
                 if cnt[nums[r]] >= max_val:
-                    max_freq.append(cnt[nums[r]])
+                    prev = nums[r]
             else:
-                if cnt[nums[l]] == max_val:
-                    max_freq.pop()
                 cnt[nums[l]] -= 1
                 l += 1
 
