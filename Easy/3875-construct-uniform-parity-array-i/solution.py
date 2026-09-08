e61e5ffd5ab3ee1c2dae7809a867ce3cec740978
kevstevie <jianna442@gmail.com>
2026-09-08 16:13:58 +0900

    feat: solve #3875 - Construct Uniform Parity Array I (Easy) [python3] [Array, Math]

---
+++ Easy/3875-construct-uniform-parity-array-i/solution.py
@@ -0,0 +1,9 @@
+# Problem: #3875 - Construct Uniform Parity Array I
+# Difficulty: Easy
+# Language: Python3
+# URL: https://leetcode.com/problems/construct-uniform-parity-array-i/
+# Submitted: 2026-09-08
+# Tags: Array, Math
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
