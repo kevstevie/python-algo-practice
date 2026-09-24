1a5205241f57753aac5f02cf8c7e1304dc5f66e6
kevstevie <jianna442@gmail.com>
2026-09-24 16:53:47 +0900

    feat: solve #3550 - Smallest Index With Digit Sum Equal to Index (Easy) [python3] [Array, Math]

--- Easy/3550-smallest-index-with-digit-sum-equal-to-index/solution.py
+++ Easy/3550-smallest-index-with-digit-sum-equal-to-index/solution.py
@@ -2,17 +2,17 @@
 # Difficulty: Easy
 # Language: Python3
 # URL: https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/
-# Submitted: 2026-07-02
+# Submitted: 2026-09-24
 # Tags: Array, Math
 class Solution:
     def smallestIndex(self, nums: List[int]) -> int:
-        def digit_sum(a):
-            res = 0
-            while a > 0:
-                res += a % 10
-                a //= 10
-            return res
+        def digit_sum(num):
+            tot = 0
+            while num > 0:
+                tot += num % 10
+                num //= 10
+            return tot
         for i in range(len(nums)):
-            if i == digit_sum(nums[i]):
+            if digit_sum(nums[i]) == i:
                 return i
         return -1
